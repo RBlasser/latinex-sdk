d7b899c320f576e1231d8a5a770ebdd44b5e1ae3
Rod Blasser <47613200+RBlasser@users.noreply.github.com>
2023-07-26 19:45:26 -0500

    Update generic_utils.py

--- latinex_sdk/generic_utils.py
+++ latinex_sdk/generic_utils.py
@@ -8,6 +8,9 @@ Created on Sat Jun  3 20:03:41 2023
 import requests
 import pandas as pd
 from urllib.parse import urljoin
+from datetime import datetime
+import pytz
+
 
 API_URL = "https://latinex-blasser-analytica.herokuapp.com/"
 
@@ -18,7 +21,9 @@ class Utilities:
     def whois():
         url = API_URL
         response = requests.get(url)
-        return response.text
+        print(response.text)
+        
+        return response.status_code
     
     def welcome():
         url = urljoin(API_URL,"welcome")
@@ -58,6 +63,36 @@ class Utilities:
                 
                 d0 = pd.DataFrame(response.json()['data_dict'])
                 d0['proc_date'] = pd.to_datetime(d0['proc_date']).dt.date
+                
+                r = d0
+        
+        
+        return r
+        
+        
+    def get_realtime(key, instrumento):
+        
+        querystring = {"instrumento":instrumento}
+        
+        headers = {"key": key} 
+        
+        url = urljoin(API_URL,"/latinex_developer/realtime_data")
+        response = requests.post(url, headers=headers, params=querystring)
+        
+        
+        if response.status_code == 200:
+            
+            if response.json()['data_dict'] == "Invalid Key":
+                
+                r =  "Invalid API Key"
+            
+            else:
+                dt = datetime.fromtimestamp(response.json()['data_dict']['timestamp']/1000.0, pytz.timezone("America/Panama")).strftime("%m/%d/%Y, %H:%M:%S")
+                date = dt.split(",")[0].strip()
+                time = dt.split(",")[1].strip()
+
+                d0 = pd.DataFrame(data={"date": date, "time": time,
+                    instrumento:float(response.json()['data_dict'][instrumento])},index=[1])
                 
                 r = d0
         
